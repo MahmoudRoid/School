ffa5cd5d013306aa7280348680f8ae8de1d3528c
Mahmoud <m.mahmoudreza1@yahoo.com>
2016-08-10 15:31:47 +0430

    initial commit

--- app/src/main/java/ir/elegam/school/Activity/MainActivity.java
+++ app/src/main/java/ir/elegam/school/Activity/MainActivity.java
@@ -1,15 +1,17 @@
 package ir.elegam.school.Activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import ir.elegam.school.R;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
