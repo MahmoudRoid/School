c939382497e2f052a66a99f9ba229086a06d2d4e
mohadi <mohamadhadibi@gmail.com>
2016-08-11 00:25:31 -0700

    mohad1

--- app/src/main/java/ir/elegam/school/Classes/URLS.java
+++ app/src/main/java/ir/elegam/school/Classes/URLS.java
@@ -6,4 +6,5 @@ public class URLS {
     public static final String WEB_SERVICE_URL = DOMAIN + "";
 
 
+
 }
---
+++ app/src/main/java/ir/elegam/school/Classes/Variables.java
@@ -0,0 +1,15 @@
+package ir.elegam.school.Classes;
+
+import android.os.Environment;
+
+public class Variables {
+
+    public static final String VERSION = "2";
+    public static final String Tag = "MYAPP";
+
+    public static final String ROOT = Environment.getExternalStorageDirectory()+"/SCHOOL";
+    public static final String PDF = ROOT + "/PDFs";
+    public static final String IMAGES = ROOT + "/images";
+
+
+}// end Variables
