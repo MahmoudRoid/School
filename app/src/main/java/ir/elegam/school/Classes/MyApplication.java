4fe2625f5a740adae385ec6d823626e348c17d5a
Mahmoud <m.mahmoudreza1@yahoo.com>
2016-08-16 08:20:17 +0430

    initial commit

--- app/src/main/java/ir/elegam/school/Classes/MyApplication.java
+++ app/src/main/java/ir/elegam/school/Classes/MyApplication.java
@@ -5,7 +5,7 @@ import android.app.Application;
 import com.orm.SugarApp;
 
 public class MyApplication extends SugarApp {
-    public String Token;
+    public String Token="";
     public boolean isNetworkconnected;
 
     @Override
